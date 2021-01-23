23cf517dc6f873516e962f61a6155f2a6ca398e5
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-23 21:01:20 +0000

    Initial Commit

---
+++ CPU/strike.v
@@ -0,0 +1,17 @@
+//-----------------------------------
+// The strike project, open source
+// 4-bit processor written in
+// Verilog and Cython
+//
+// strike.v - Main file
+//-----------------------------------
+
+module strike_CPU();
+
+initial begin
+	$display ("Hello, world!");
+	$finish;
+end
+
+
+endmodule
